b9ccd3bb78dcf56e1f48b0ac0eac2f81f8ca05ef
LoveSSJ <liu.wen.guang@qq.com>
2019-02-12 23:55:44 +0800

    2019.2.12 DoTween动画插件学习 插件导入 简单线性动画移动控制

---
+++ DoTweenProject/Assets/Scripts/GetStart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GetStart : MonoBehaviour
+{
+    public Vector3 myValue = new Vector3(0, 0, 0);
+
+    public Transform cubeTransform;
+
+    public RectTransform taskPanelTransform;
+
+    public float myValue2 = 0;
+    // Use this for initialization
+    void Start()
+    {
+        DOTween.To(() => myValue, x => myValue = x, new Vector3(0, 0, 0), 2.0f);
+        DOTween.To(() => myValue2, x => myValue2 = x, 10, 2);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //cubeTransform.position = myValue;
+        taskPanelTransform.localPosition = myValue;
+    }
+}
