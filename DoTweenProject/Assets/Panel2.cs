791b3d8b4e0a7199cb15aff6dbd256b18a6ce59f
LoveSSJ <liu.wen.guang@qq.com>
2019-02-14 23:35:39 +0800

    2019.2.14 DoTween动画插件常用的了解完了  创了一个秘密行动的项目准备先把动画学完再学 开始Unity中的动画系统和Timeline(创建动画，编辑动画，动画控制物体position)

---
+++ DoTweenProject/Assets/Panel2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Panel2 : MonoBehaviour {
+
+    private DOTweenAnimation animation;
+    private bool isShow = false;
+
+	// Use this for initialization
+	void Start () {
+        animation = this.GetComponent<DOTweenAnimation>();
+        //animation.DOPlay();
+
+    }
+	
+	public void OnClick()
+    {
+        if (isShow)
+        {
+            animation.DOPlayBackwards();
+            isShow = false;
+        }
+        else
+        {
+            animation.DOPlayForward();
+            isShow = true;
+        }
+    }
+}
---
+++ MoveEffectTool(Dotween)/Assets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //rotate
+        //transform.DOLookAt(Vector3.one, 2);
+
+        //scale
+        //transform.DOScale(2, 1);
+
+        transform.DOPunchPosition(new Vector3(0, 1, 0), 2, 2, 1);
+
+	}
+}
