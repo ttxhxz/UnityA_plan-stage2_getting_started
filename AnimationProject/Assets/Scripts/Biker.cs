548da4e19394e9db89ed7e5d6ea2ab1acd025567
LoveSSJ <liu.wen.guang@qq.com>
2019-02-17 23:44:49 +0800

    2019.2.17分割动画，通过代码控制状态机 Generic和Humanid导入方式动画的区别。。。骨骼映射，Avatar创建

---
+++ AnimationProject/Assets/Scripts/Biker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biker : MonoBehaviour
+{
+
+    private Animator ani;
+    // Use this for initialization
+    void Start()
+    {
+        ani = this.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        ani.SetInteger("Vertical", (int)v);
+        //ani.SetFloat("Vertical01", v);
+
+        Debug.Log("v:" + v + "\t " + (int)v);
+
+    }
+}
