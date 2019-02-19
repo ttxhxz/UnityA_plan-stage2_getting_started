9e5ff46f1ed92bbf3e00945961183b4a6b8655e0
LoveSSJ <liu.wen.guang@qq.com>
2019-02-19 23:58:34 +0800

    2019.2.19 元宵快乐 导入官方角色模型 配置角色动画导入 控制人物行走，左右转 stringtohash

---
+++ AnimationProject/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int verticalId = Animator.StringToHash("Vertical");
+    private int horizontalId = Animator.StringToHash("Horizontal");
+    private int IsSpeedUpId = Animator.StringToHash("IsSpeedUp");
+
+    private Animator ani;
+    // Use this for initialization
+    void Start()
+    {
+        ani = this.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ani.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        ani.SetFloat(verticalId, Input.GetAxis("Vertical"));//StringToHash把参数字符串转换成ID 前后
+        ani.SetFloat(horizontalId, Input.GetAxis("Horizontal"));//左右
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            ani.SetBool(IsSpeedUpId, true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            ani.SetBool(IsSpeedUpId, false);
+        }
+    }
+}
