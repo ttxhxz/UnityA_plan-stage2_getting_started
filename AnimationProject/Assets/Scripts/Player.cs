4aec6d802e824b22ed6eca231d6051fdfb5f47c4
LoveSSJ <liu.wen.guang@qq.com>
2019-02-21 23:59:52 +0800

    2019.2.21 控制摄像机跟随，检测障碍物方案，角色翻墙动画播放，设置起跳距离 通过MatchTarget修改翻墙动画 优化动画匹配度，添加角色控制器，通过动画的数值控制玩家的角色控制器的禁用和启用

--- AnimationProject/Assets/Scripts/Player.cs
+++ AnimationProject/Assets/Scripts/Player.cs
@@ -10,20 +10,84 @@ public class Player : MonoBehaviour
 
     private int SpeedRotateId = Animator.StringToHash("SpeedRotate");
     private int SpeedZId = Animator.StringToHash("SpeedZ");
+    private int VaultId = Animator.StringToHash("Vault");
+    private int SliderId = Animator.StringToHash("Slider");
+
+    private int ColliderId = Animator.StringToHash("Collider");
+
+    public Vector3 matchTarget;
 
     private Animator ani;
-    // Use this for initialization
+    private CharacterController controller;
+
     void Start()
     {
         ani = this.GetComponent<Animator>();
-
+        controller = this.GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         ani.SetFloat(SpeedRotateId, Input.GetAxis("Horizontal") * 126);
         ani.SetFloat(SpeedZId, Input.GetAxis("Vertical") * 4.1f);
+
+        //翻墙动作
+        bool isVault = false;
+        //当速度大于3并且动画状态在第0层的Localmotion的时候才判断是否起跳
+        if (SpeedZId > 3 && ani.GetCurrentAnimatorStateInfo(0).IsName("Localmotion"))
+        {
+            RaycastHit hit;
+            bool isHit = Physics.Raycast(transform.position + Vector3.up * 0.3f, transform.forward, out hit, 4f);
+            if (isHit && hit.collider.tag == "Obstacle")
+            {
+                if (hit.distance > 3f)
+                {
+                    isVault = true;
+                    Vector3 point = hit.point;
+                    //重置碰撞点的y为  (hit.transform.position.y + hit.collider.bounds.size.y) 代表障碍物的初始y值加上障碍物的y
+                    point.y = (hit.transform.position.y + hit.collider.bounds.size.y) + 0.11f;
+                    //为了扶到墙的中间，而不是墙的边缘加上墙的z的一半
+                    point.z = hit.transform.position.z + hit.collider.bounds.size.z / 2;
+                    matchTarget = point;
+                }
+            }
+        }
+        ani.SetBool(VaultId, isVault);
+
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("Vault") && ani.IsInTransition(0) == false)
+        {
+            ani.MatchTarget(matchTarget, Quaternion.identity, AvatarTarget.LeftHand, new MatchTargetWeightMask(Vector3.one, 0), 0.32f, 0.4f);
+
+            controller.enabled = ani.GetFloat(ColliderId) <= 0.5;
+        }
+
+        //滑行动作
+        bool isSlider = false;
+        if (SpeedZId > 3 && ani.GetCurrentAnimatorStateInfo(0).IsName("Localmotion"))
+        {
+            RaycastHit hit;
+            bool isHit = Physics.Raycast(transform.position + Vector3.up, transform.forward, out hit, 4.0f);
+            if (isHit && hit.collider.tag == "Obstacle")
+            {
+                if (hit.distance > 3)
+                {
+                    isSlider = true;
+                }
+            }
+        }
+        ani.SetBool(SliderId, isSlider);
+        //滑行动作取消controller
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("Slider") && ani.IsInTransition(0) == false)
+        {
+            controller.enabled = ani.GetFloat(ColliderId) <= 0.5;
+        }
+
+
+
+
+
+
 
         //ani.SetFloat("Vertical", Input.GetAxis("Vertical"));
         //ani.SetFloat(verticalId, Input.GetAxis("Vertical"));//StringToHash把参数字符串转换成ID 前后
