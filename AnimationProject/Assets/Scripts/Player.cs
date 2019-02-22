4150c0ac0d0ee4dc4d57bfc5a013cc323a89811a
LoveSSJ <liu.wen.guang@qq.com>
2019-02-22 23:41:11 +0800

    2019.2.22 添加滑动动画 控制滑动动画下滑穿过障碍物 通过matchtarget进行匹配优化滑动效果

--- AnimationProject/Assets/Scripts/Player.cs
+++ AnimationProject/Assets/Scripts/Player.cs
@@ -33,6 +33,36 @@ public class Player : MonoBehaviour
         ani.SetFloat(SpeedZId, Input.GetAxis("Vertical") * 4.1f);
 
         //翻墙动作
+        ProcessVault();
+
+        //滑行动作
+        ProcessSlider();
+
+
+        #region 注释代码
+
+        //ani.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        //ani.SetFloat(verticalId, Input.GetAxis("Vertical"));//StringToHash把参数字符串转换成ID 前后
+        //ani.SetFloat(horizontalId, Input.GetAxis("Horizontal"));//左右
+
+        //if (Input.GetKeyDown(KeyCode.LeftShift))
+        //{
+        //    ani.SetBool(IsSpeedUpId, true);
+        //}
+
+        //if (Input.GetKeyUp(KeyCode.LeftShift))
+        //{
+        //    ani.SetBool(IsSpeedUpId, false);
+        //}
+
+        //ani.SetFloat(verticalId, Input.GetAxis("Vertical") * 4.1f);//StringToHash把参数字符串转换成ID 前后
+        #endregion
+
+    }
+
+    //翻墙动作
+    private void ProcessVault()
+    {
         bool isVault = false;
         //当速度大于3并且动画状态在第0层的Localmotion的时候才判断是否起跳
         if (SpeedZId > 3 && ani.GetCurrentAnimatorStateInfo(0).IsName("Localmotion"))
@@ -61,18 +91,23 @@ public class Player : MonoBehaviour
 
             controller.enabled = ani.GetFloat(ColliderId) <= 0.5;
         }
+    }
 
-        //滑行动作
+    //滑行动作
+    private void ProcessSlider()
+    {
         bool isSlider = false;
         if (SpeedZId > 3 && ani.GetCurrentAnimatorStateInfo(0).IsName("Localmotion"))
         {
             RaycastHit hit;
-            bool isHit = Physics.Raycast(transform.position + Vector3.up, transform.forward, out hit, 4.0f);
+            bool isHit = Physics.Raycast(transform.position + Vector3.up * 1.5f, transform.forward, out hit, 3.0f);
             if (isHit && hit.collider.tag == "Obstacle")
             {
-                if (hit.distance > 3)
+                if (hit.distance > 2)
                 {
                     isSlider = true;
+                    Vector3 point = hit.point;
+                    matchTarget = point + transform.forward * 2.0f;
                 }
             }
         }
@@ -80,30 +115,8 @@ public class Player : MonoBehaviour
         //滑行动作取消controller
         if (ani.GetCurrentAnimatorStateInfo(0).IsName("Slider") && ani.IsInTransition(0) == false)
         {
+            ani.MatchTarget(matchTarget, Quaternion.identity, AvatarTarget.LeftHand, new MatchTargetWeightMask(new Vector3(1, 0, 1), 0), 0.16f, 0.67f);
             controller.enabled = ani.GetFloat(ColliderId) <= 0.5;
         }
-
-
-
-
-
-
-
-        //ani.SetFloat("Vertical", Input.GetAxis("Vertical"));
-        //ani.SetFloat(verticalId, Input.GetAxis("Vertical"));//StringToHash把参数字符串转换成ID 前后
-        //ani.SetFloat(horizontalId, Input.GetAxis("Horizontal"));//左右
-
-        //if (Input.GetKeyDown(KeyCode.LeftShift))
-        //{
-        //    ani.SetBool(IsSpeedUpId, true);
-        //}
-
-        //if (Input.GetKeyUp(KeyCode.LeftShift))
-        //{
-        //    ani.SetBool(IsSpeedUpId, false);
-        //}
-
-        //ani.SetFloat(verticalId, Input.GetAxis("Vertical") * 4.1f);//StringToHash把参数字符串转换成ID 前后
-
     }
 }
