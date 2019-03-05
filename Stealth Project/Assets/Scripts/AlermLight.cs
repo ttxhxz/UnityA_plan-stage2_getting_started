7efb06dd037d046d43ad1db3bb7c8bbc73b53c05
LoveSSJ <liu.wen.guang@qq.com>
2019-03-05 23:44:45 +0800

    2019.3.5 添加警报灯，已经警报灯光闪烁效果

---
+++ Stealth Project/Assets/Scripts/AlermLight.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlermLight : MonoBehaviour
+{
+    public bool alermOn;
+
+    private float lowIntensity = 0f;
+    private float hightIntensity = 1f;
+    private float targetIntensity;
+
+    public float animationSpeed = 3f;
+    private Light light;
+
+    private void Awake()
+    {
+        alermOn = false;
+        targetIntensity = hightIntensity;
+        light = this.GetComponent<Light>();
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alermOn)
+        {
+            light.intensity = Mathf.Lerp(light.intensity, targetIntensity, Time.deltaTime * animationSpeed);
+            if (Mathf.Abs(light.intensity - targetIntensity) < 0.05f)
+            {
+                if (targetIntensity == hightIntensity)
+                {
+                    targetIntensity = lowIntensity;
+                }
+                else if (targetIntensity == lowIntensity)
+                {
+                    targetIntensity = hightIntensity;
+                }
+            }
+        }
+        else
+        {
+            light.intensity = Mathf.Lerp(light.intensity, lowIntensity, Time.deltaTime * animationSpeed);
+        }
+    }
+}
