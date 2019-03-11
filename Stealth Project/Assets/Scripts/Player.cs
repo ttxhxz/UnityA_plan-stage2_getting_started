d4ea611238f84e178b3e302eaf2445bcdb30dea1
LoveSSJ <liu.wen.guang@qq.com>
2019-03-11 23:44:24 +0800

    2019.3.11 添加脚本

---
+++ Stealth Project/Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 3;
+    public float stopSpeed = 7;
+
+}
