2d32b9d9b44c2c05e41b6f34d1ba8c68b52346ef
LoveSSJ <liu.wen.guang@qq.com>
2019-03-13 23:03:59 +0800

    2019.3.13 添加自动门，添加电梯门，设置内侧和外侧打开门

---
+++ Stealth Project/Assets/Scripts/Door.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    private int closeId = Animator.StringToHash("Close");
+    private Animator anim;
+    private AudioSource audio;
+    private int count = 0;//在Collider中的人物数量
+
+    private void Awake()
+    {
+        anim = this.GetComponent<Animator>();
+        audio = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool(closeId, count <= 0);
+        if (anim.IsInTransition(0))
+        {
+            if (!audio.isPlaying)
+            {
+                audio.Play();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == Tags.enemy || other.tag == Tags.player)
+        {
+            count++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == Tags.enemy || other.tag == Tags.player)
+        {
+            count--;
+        }
+    }
+}
