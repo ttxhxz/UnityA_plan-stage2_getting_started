2dc30efd6730cd6efa35d0ff42c54b3ce1733cec
LoveSSJ <liu.wen.guang@qq.com>
2019-04-09 23:07:24 +0800

    2019.4.9 敌人动画设置，转动设置

--- Stealth Project/Assets/Scripts/Enemy/EnemyAnimation.cs
+++ Stealth Project/Assets/Scripts/Enemy/EnemyAnimation.cs
@@ -5,6 +5,85 @@ using UnityEngine.AI;
 
 public class EnemyAnimation : MonoBehaviour
 {
+    public float deadZone = 5f;
+
+    private Transform player;
+    private EnemySight enemySight;
+    private NavMeshAgent nav;
+    private Animator anim;
+    private HashIDs hash;
+    private AnimatorSetup animSetup;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.player).transform;
+        enemySight = GetComponent<EnemySight>();
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        hash = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<HashIDs>();
+
+        nav.updateRotation = false;
+        animSetup = new AnimatorSetup(anim, hash);
+
+        anim.SetLayerWeight(1, 1f);
+        anim.SetLayerWeight(2, 1f);
+
+        //从度转化为弧度
+        deadZone *= Mathf.Deg2Rad;
+    }
+
+    private void Update()
+    {
+        NavAnimSetup();
+    }
+
+    private void OnAnimatorMove()
+    {
+        nav.velocity = anim.deltaPosition / Time.deltaTime;
+        transform.rotation = anim.rootRotation;
+    }
+
+    void NavAnimSetup()
+    {
+        float speed;
+        float angle;
+
+        if (enemySight.playerInSight)
+        {
+            speed = 0f;
+            angle = FindAngle(transform.forward, player.position - transform.position, transform.up);
+        }
+        else
+        {
+            speed = Vector3.Project(nav.desiredVelocity, transform.forward).magnitude;
+
+            angle = FindAngle(transform.forward, nav.desiredVelocity, transform.up);
+
+            if (Mathf.Abs(angle) < deadZone)
+            {
+                transform.LookAt(transform.position + nav.desiredVelocity);
+                angle = 0f;
+            }
+        }
+
+        animSetup.Setup(speed, angle);
+    }
+
+    float FindAngle(Vector3 fromVector, Vector3 toVector, Vector3 upVector)
+    {
+        if (toVector == Vector3.zero)
+        {
+            return 0f;
+        }
+
+        float angle = Vector3.Angle(fromVector, toVector);
+        Vector3 normal = Vector3.Cross(fromVector, toVector);
+        //Mathf.Sign当参数为正或零时，返回值为1，当参数为负时，返回值为-1。
+        angle *= Mathf.Sign(Vector3.Dot(normal, upVector));
+        angle *= Mathf.Deg2Rad;
+
+        return angle;
+    }
 
     #region 注释代码
     //private NavMeshAgent navAgent;
--- Stealth Project/Assets/Scripts/Unapplied/AnimatorSetup.cs
+++ Stealth Project/Assets/Scripts/Unapplied/AnimatorSetup.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class AnimatorSetup
 {
     public float speedDampTime = 0.1f;
-    public float angularSpeedDampTime = 0.3f;
+    public float angularSpeedDampTime = 0.7f;
     public float angleResponseTime = 0.6f;
 
     private Animator anim;
-    //private HashIDs hash;
+    private HashIDs hash;
+
+    public AnimatorSetup(Animator animator, HashIDs hashIDs)
+    {
+        anim = animator;
+        hash = hashIDs;
+    }
+
+    public void Setup(float speed, float angle)
+    {
+        float angularSpeed = angle / angleResponseTime;
+
+        anim.SetFloat(hash.speedFloat, speed, speedDampTime, Time.deltaTime);
+        anim.SetFloat(hash.angularSpeedFloat, angularSpeed, angularSpeedDampTime, Time.deltaTime);
+    }
 }
